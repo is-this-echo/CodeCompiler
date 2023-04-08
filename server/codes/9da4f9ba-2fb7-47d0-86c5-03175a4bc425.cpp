68e00c7edd2546772b3ba4d2310a69e34bdfd470
Debjyoti <djpool007@gmail.com>
2023-04-08 12:02:37 +0530

    added backend code

--- server/codes/9da4f9ba-2fb7-47d0-86c5-03175a4bc425.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-int main(){
-string s="igig";
-cout<<s;
-return 0;
-}
--- server/codes/d461fc99-532a-4f38-8ac9-99100c6c042d.cpp
+++
@@ -1,3 +0,0 @@
-    const { data } = await axios.post("http://localhost:5000/run", payload);
-
-    setOutput(data.output);
